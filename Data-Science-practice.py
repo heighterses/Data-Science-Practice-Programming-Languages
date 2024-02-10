074bbab1c6ab6550cd6ab07cbfcfd7b888672f85
M. Abdullah Bin Aftab <110668234+heighterses@users.noreply.github.com>
2024-02-11 03:15:37 +0500

    Update Data-Science-practice.py

--- Data-Science-practice.py
+++ Data-Science-practice.py
@@ -17,12 +17,19 @@ reshaped_df.isna().values.any()
 
 # making visuals of data with matplotlib
 
+# plt.plot(reshaped_df.index, reshaped_df['python'], reshaped_df['java'])
+
+roll_df = reshaped_df.rolling(window=6).mean()
+
+
 plt.figure(figsize=(16,10))
 plt.xticks(fontsize=14)
 plt.yticks(fontsize=14)
 plt.xlabel('Date', fontsize=14)
 plt.ylabel('Number of Posts', fontsize=14)
 plt.ylim(0, 35000)
-for column in reshaped_df.columns:
-  plt.plot(reshaped_df.index, reshaped_df[column], linewidth = 3, label = reshaped_df[column].name )
+
+for column in roll_df.columns:
+    plt.plot(roll_df.index, roll_df[column], 
+             linewidth=3, label=roll_df[column].name)
 plt.legend(fontsize=11) 
