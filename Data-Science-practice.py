abd0641d47c56f821697bb59e9bd567aacbcc773
M. Abdullah Bin Aftab <110668234+heighterses@users.noreply.github.com>
2024-02-11 03:10:23 +0500

    Update Data-Science-practice.py

--- Data-Science-practice.py
+++ Data-Science-practice.py
@@ -1,1 +1,28 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+df = pd.read_csv('QueryResults.csv')
+df.head()
+df.columns=["Date","Tag","Post"]
+df.head()
+df.shape
+df[['Date','Tag','Post']].count()
+df.groupby('Tag').sum()
+df.Date = pd.to_datetime(df.Date)
+reshaped_df = df.pivot(index='Date', columns='Tag', values='Post')
+reshaped_df.head()
+reshaped_df.fillna(0, inplace=True)
+reshaped_df.count()
+reshaped_df.isna().values.any()
+# plt.plot(reshaped_df.index, reshaped_df['python'], reshaped_df['java'])
 
+# making visuals of data with matplotlib
+
+plt.figure(figsize=(16,10))
+plt.xticks(fontsize=14)
+plt.yticks(fontsize=14)
+plt.xlabel('Date', fontsize=14)
+plt.ylabel('Number of Posts', fontsize=14)
+plt.ylim(0, 35000)
+for column in reshaped_df.columns:
+  plt.plot(reshaped_df.index, reshaped_df[column], linewidth = 3, label = reshaped_df[column].name )
+plt.legend(fontsize=11) 
